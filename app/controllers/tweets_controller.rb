b54c4960a087a72d4dfec8663183c85042a5d260
camilaphenderson <cvpellegrini@miuandes.cl>
2023-06-10 16:21:00 -0400

    boton crear

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,4 +10,20 @@ class TweetsController < ApplicationController
     def new
         @tweet = Tweet.new
     end
+
+    def create
+        @tweet = Tweet.new tweets_params
+        tweet.user= current_user
+        if tweet.save!
+            redirect_to tweet, notice: 'Tweet guardado'
+        else 
+            render :new
+        end
+    end
+
+    private
+
+    def tweets_params
+        params.require(:tweet).permit(:body)
+    end
 end
